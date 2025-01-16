8c6ceacd48a319e9f62393b8a2c97a4e5d5981bf
sonnnnnnp <sonnnnnnp2@gmail.com>
2025-01-16 23:29:57 +0900

    DMのテーブル追加

--- server/migrations/000001_init.up.sql
+++ server/migrations/000001_init.up.sql
@@ -87,3 +87,64 @@ CREATE TABLE IF NOT EXISTS call_participants (
     left_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
     PRIMARY KEY (call_id, participant_id)
 );
+
+-- messages
+
+CREATE TYPE rooms_type AS ENUM (
+    'individual',
+    'group'
+);
+
+CREATE TABLE IF NOT EXISTS rooms (
+    id SERIAL PRIMARY KEY,
+    rooms_type rooms_type, -- individual | group
+    name VARCHAR(100), -- 個人チャット(individual)の場合はNULL
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS room_members (
+    id SERIAL PRIMARY KEY,
+    rooms_id INT NOT NULL REFERENCES rooms(id),
+    user_id UUID NOT NULL REFERENCES users(id),
+    CONSTRAINT unique_user_room UNIQUE (user_id, rooms_id) --　ユーザーとルームの組み合わせを一意に
+);
+
+CREATE TABLE IF NOT EXISTS pinned_rooms (
+    id SERIAL PRIMARY KEY,
+    user_id NOT NULL REFERENCES users(id),
+    rooms_id NOT NULL REFERENCES rooms(id),
+    pinned_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    CONSTRAINT unique_user_room UNIQUE (user_id, rooms_id) --　ユーザーとルームの組み合わせを一意に
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    room_id NOT NULL REFERENCES rooms(id),
+    sender_id UUID NOT NULL REFERENCES users(id),
+    content VARCHAR(10000),
+    has_file BOOLEAN DEFAULT FALSE,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS message_reads (
+    id SERIAL PRIMARY KEY,
+    message_id NOT NULL REFERENCES messages(id),
+    user_id UUID NOT NULL REFERENCES users(id),
+    read_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TYPE file_type AS ENUM (
+    'image',
+    'video',
+    'voice'
+);
+
+CREATE TABLE IF NOT EXISTS message_files (
+    id SERIAL PRIMARY KEY,
+    message_id INT NOT NULL REFERENCES messages(id) ON DELETE CASCADE,
+    file_url TEXT NOT NULL,
+    file_type file_type,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
